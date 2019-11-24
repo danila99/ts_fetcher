7690e3689c3c9b6cdedcacd84424cfd82ac4cc79
Konstantin Danilov <kdanilov@gmail.com>
2019-11-24 16:18:55 +0100

    allowing postfixes to the url composition. Extracting VideoChunk class for obvious reasons.

--- ts_fetcher.py
+++ ts_fetcher.py
@@ -9,45 +9,47 @@ MAX_FORESEEABLE_INDEX = 10000
 MAX_WORKERS = 32
 
 
-def get_url(base_url, index):
-    return f"{base_url}{index}.ts"
+class VideoChunk(object):
+    def __init__(self, base_url, postfix_url):
+        self.base = base_url
+        self.postfix = postfix_url
 
+    def url(self, i):
+        return f"{self.base}{i}.ts{self.postfix}"
 
-def file_exists(base_url, index):
-    url = get_url(base_url, index)
-    req = urllib.request.Request(url, method="HEAD")
-    print(f"looking for: {url}...", end=" ")
-    try:
-        with urllib.request.urlopen(req) as response:
-            print("found")
-            return response.getcode() == 200
-    except Exception as ex:
-        print(f"not found: {ex}")
-        return False
+    def exists(self, i):
+        url = self.url(i)
+        req = urllib.request.Request(url, method="HEAD")
+        print(f"looking for: {url}...", end=" ")
+        try:
+            with urllib.request.urlopen(req) as response:
+                print("found")
+                return response.getcode() == 200
+        except Exception as ex:
+            print(f"not found: {ex}")
+            return False
 
+    def get_max_index(self, left, right):
+        while left + 1 < right:
+            index = left + (right - left) // 2
+            if self.exists(index):
+                left = index
+            else:
+                right = index
 
-def get_max_index(base_url, left, right):
-    while left + 1 < right:
-        index = left + (right - left) // 2
-        if file_exists(base_url, index):
-            left = index
+        if self.exists(left + 1):
+            return left + 1
         else:
-            right = index
+            return left
 
-    if file_exists(base_url, left + 1):
-        return left + 1
-    else:
-        return left
-
-
-def save_locally(base_url, base_folder, i):
-    file_name = os.path.join(base_folder, f"{i}.ts")
-    url = get_url(base_url, i)
-    # TODO: treat unexpected HTTP exceptions here
-    with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
-        print(f"writing to: {file_name}...")
-        shutil.copyfileobj(response, out_file)
-        return f"done: {file_name}"
+    def save_locally(self, base_folder, i):
+        file_name = os.path.join(base_folder, f"{i}.ts")
+        url = self.url(i)
+        # TODO: treat unexpected HTTP exceptions here
+        with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+            print(f"writing to: {file_name}...")
+            shutil.copyfileobj(response, out_file)
+            return f"done: {file_name}"
 
 
 def prepare_base_url(url):
@@ -56,10 +58,11 @@ def prepare_base_url(url):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Download all *.ts files for video which was split with indices"
+        description="Download all *.ts files for remote video resource"
     )
-    parser.add_argument('url', help="URL part before '{index}.ts'")
-    parser.add_argument('folder', help="destination folder", default=".")
+    parser.add_argument('url', help="URL part before '{index}.ts'", type=str)
+    parser.add_argument('folder', help="destination folder", default=".", type=str)
+    parser.add_argument('url_postfix', help="URL part after '{index}.ts' (optional)", default="", type=str)
     args = parser.parse_args()
 
     if not os.path.exists(args.folder):
@@ -67,20 +70,21 @@ if __name__ == "__main__":
 
     base_folder = args.folder
     base_url = prepare_base_url(args.url)
+    video_chunk = VideoChunk(base_url, args.url_postfix)
 
-    print(f"will download from: {base_url}")
+    print(f"will download from: {video_chunk.url(1)}")
     print(f"will write to: {base_folder}")
 
-    if not file_exists(base_url, 1):
-        raise Exception(f"no resource at {get_url(base_url, 1)}")
+    if not video_chunk.exists(1):
+        raise Exception(f"no resource at {video_chunk.url(1)}")
 
-    max_index = get_max_index(base_url, 1, MAX_FORESEEABLE_INDEX)
+    max_index = video_chunk.get_max_index(1, MAX_FORESEEABLE_INDEX)
     all_indices = list(range(1, max_index + 1))
     print(f"max index found: {max_index}")
 
     # fetching files from the website
     with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        future_to_url = {executor.submit(save_locally, base_url, base_folder, i): i for i in all_indices}
+        future_to_url = {executor.submit(video_chunk.save_locally, base_folder, i): i for i in all_indices}
         for future in concurrent.futures.as_completed(future_to_url):
             url = future_to_url[future]
             try:
